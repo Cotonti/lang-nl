f27f82eee8f5d3eb51062932d49b93168cac71b5
Twiebie <admin@twiebie.com>
2011-09-03 21:12:14 +0200

    Upgrade to 0.9.4

---
+++ informal/modules/users/lang/users.nl.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Dutch Language File for the Users Module
+ *
+ * @package users
+ * @version 0.9.4
+ * @author Cotonti Translators Team
+ * @copyright Copyright (c) Cotonti Team 2011
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['info_desc'] = 'Activeert leden registratie, profielen en lijsten op jouw website';
+
+$L['cfg_disablereg'] = array('Deactiveer registratie proces', 'Mogelijkheid voor registratie uitschakelen');
+$L['cfg_maxusersperpage'] = array('Maximale aantal regels in ledenlijst', '');
+$L['cfg_regnoactivation'] = array('Skip e-mail check voor nieuwe leden', '\'Nee\' aanbevolen wegens beveiligings redenen');
+$L['cfg_regrequireadmin'] = array('Administratoren moeten nieuwe gebruikers valideren', '');
+$L['cfg_user_email_noprotection'] = array('Schakel wachtwoord beveiliging uit bij e-mail wijziging', '\'Nee\' aanbevolen wegens beveiligings redenen');
+$L['cfg_useremailchange'] = array('Sta gebruikers toe wachtwoord te wijzigen', '\'Nee\' aanbevolen wegens beveiligings redenen');
+$L['cfg_usertextmax'] = array('Maximale lengte voor gebruikers handtekening', '');
+
+?>
