ea41a350bb7da7d0b2b8899dd04a508564d819e1
Twiebie <twiebie@twiebie.com>
2012-05-29 23:31:25 +0200

    Dutch language files for Sitemap plugin

---
+++ informal/plugins/sitemap/lang/sitemap.nl.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dutch Language File for SiteXML plugin
+ *
+ * @package sitemap
+ * @author Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2012
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['cfg_perpage'] = array('Max items per sitemap pagina', 'Sitemaps worden in delen gesplitst wanneer er meerdere links zijn, zie http://yoursite/index.php?r=sitemap&a=index');
+
+?>
